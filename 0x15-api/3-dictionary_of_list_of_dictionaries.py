55ee60b63c38483a747a43e268bdae8600da8164
Ridwan lawal <lawalridwan007@gmail.com>
2023-07-25 14:59:46 +0000

    correct pycodestyle

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,7 +6,6 @@ import requests
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-    
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             u.get("id"): [{
@@ -14,5 +13,5 @@ if __name__ == "__main__":
                 "completed": t.get("completed"),
                 "username": u.get("username")
                 } for t in requests.get(url + "todos",
-                    params={"userId": u.get("id")}).json()]
+                                        params={"userId": u.get("id")}).json()]
                 for u in users}, jsonfile)
